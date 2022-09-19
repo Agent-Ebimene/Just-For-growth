dc657a1731095f2ed49723bad2cf4071b4108772
Agent-Ebimene <77029524+Agent-Ebimene@users.noreply.github.com>
2022-09-19 09:51:51 +0100

    Create Connecting React to Node and Express.js
    This further research was to help me grow in my knowledge of backend

---
+++ Connecting React to Node and Express.js
@@ -0,0 +1,5 @@
+I checked out how to connect Node js and Express to an A react Js Application.
+Created a server and a basic API that sends the client side a few Data.
+Used the useEffect React Method and the fetch API to get this data.
+Then,the fetched result was displayed in the browser through a state variable that was set to hold the returned value.
+This Youtube resource made this whole process way easier
